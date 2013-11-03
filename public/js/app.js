4da4b2bc0c3f64a1342ad7b36301372d12f80166
smotchkkiss <smotchkkiss@gmail.com>
2013-11-03 15:44:30 -0600

    refactor layout/header a little bit

--- public/js/app.js
+++ public/js/app.js
@@ -132,19 +132,23 @@ var HeaderView = module.exports = Backbone.View.extend({
   id: "header",
 
   render: function() {
+    this.$el.append(this.banner());
+    this.$el.append(this.todoInput());
+    return this;
+  },
 
+  banner: function() {
     var h1 = document.createElement("h1");
     h1.appendChild(document.createTextNode("Skellington"));
+    return h1;
+  },
 
+  todoInput: function() {
     var input = document.createElement("input");
     input.id          = "new-todo";
     input.placeholder = "What needs to be done?";
     input.autofocus   = true;
-
-    this.$el.append(h1);
-    this.$el.append(input);
-
-    return this;
+    return input;
   },
 
 });
